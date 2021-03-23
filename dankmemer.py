8e168e1662b7fedf0559af03f7418509aef2c4fa
Shadow7258 <pranav.rdoshi@gmail.com>
2021-03-23 16:20:11 +0530

    Updated script

--- dankmemer.py
+++ dankmemer.py
@@ -4,7 +4,11 @@ a=10000
 b=0
 time.sleep(3)
 while(a>b):
-   pyautogui.write("pls fish")
+   pyautogui.typewrite("pls fish")
+   pyautogui.press('enter')
+   pyautogui.typewrite("pls beg")
+   pyautogui.press('enter')      
+   pyautogui.typewrite("pls dep all")
    pyautogui.press('enter')   
-   time.sleep(40)
+   time.sleep(45)
    b=b+1
